6c700455acb5a5f0327f4ba50ba48b25ec2017a0
Dener Almeida <dener_bsb@live.com>
2023-01-24 13:35:32 -0300

    added 'use strict'

--- 8 Kyu/even_of_odd.js
+++ 8 Kyu/even_of_odd.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 //My solution
--- 8 Kyu/price_of_mangoes.js
+++ 8 Kyu/price_of_mangoes.js
@@ -1,3 +1,4 @@
+'use strict';
 /* 
 
 There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
