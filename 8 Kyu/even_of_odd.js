d5f6b4462d27a717c2126f604683ca0869d1b7cb
Dener Almeida <dener_bsb@live.com>
2023-01-24 13:16:20 -0300

    Added new Codewars challenge

---
+++ 8 Kyu/even_of_odd.js
@@ -0,0 +1,8 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//My solution
+function evenOrOdd(number) {
+  return number % 2 === 0 ? console.log('Even') : console.log('Odd');
+}
+
+evenOrOdd(3);
