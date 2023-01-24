7e5bef1d07701ff9a1634c70780469b8c8e7dc58
Dener Almeida <dener_bsb@live.com>
2023-01-24 13:49:25 -0300

    Update vowel_remover.js
    Added a solution that doesn't use regex

--- 8 Kyu/vowel_remover.js
+++ 8 Kyu/vowel_remover.js
@@ -2,8 +2,29 @@
 
 //Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
 
+//Using regex
 function shortcut(str) {
   return str.replace(/[aeiouy]/gi, '');
 }
 
 console.log(shortcut('string'));
+
+//Without regex
+function anotherShortcut(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] != 'a' &&
+      str[i] != 'e' &&
+      str[i] != 'i' &&
+      str[i] != 'o' &&
+      str[i] != 'u' &&
+      str[i] != 'y'
+    ) {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
+
+console.log(anotherShortcut('stringy'));
