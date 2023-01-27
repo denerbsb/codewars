d2bf79df05732be921fd2466487ff894711fcd5c
Dener Almeida <dener_bsb@live.com>
2023-01-27 14:35:03 -0300

    added new challenge

---
+++ 6 Kyu/count_smiles.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ';-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+*/
+
+//My solution
+
+function countSmiles(arr) {
+  let newArr = [];
+
+  arr.forEach((element) => {
+    if (
+      element === ':)' ||
+      element === ':D' ||
+      element === ';-D' ||
+      element === ':~)'
+    ) {
+      newArr.push(element);
+    } else if (arr === []) {
+      return 0;
+    }
+  });
+
+  return newArr.length;
+}
+
+console.log(countSmiles([':D', ':~)', ';~D', ':)']));
