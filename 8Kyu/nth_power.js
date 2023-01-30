f02263c396ea249290b4082007e19dc2f50021cd
Dener Almeida <dener_bsb@live.com>
2023-01-30 08:40:55 -0300

    added commentary

--- 8Kyu/nth_power.js
+++ 8Kyu/nth_power.js
@@ -11,6 +11,7 @@ array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 */
 
+//My solution
 function index(array, n) {
   return array[n] !== undefined ? Math.pow(array[n], n) : -1;
 }
